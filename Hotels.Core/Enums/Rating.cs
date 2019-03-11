58562a9e06436035d6481d1dd4378a56b4335858
Bodrul <bodrul.ic@msn.com>
2019-03-11 15:48:17 +0000

    Added rating enum.

---
+++ Hotels.Core/Enums/Rating.cs
@@ -0,0 +1,14 @@
+﻿namespace Hotels.Core.Enums
+{
+    /// <summary>
+    /// The rating class. The higher the rating, the better the hotel.
+    /// </summary>
+    public enum Rating
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5
+    }
+}
