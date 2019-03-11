99c1778bb16b9da94eb6e43c702b04879c28b68a
Bodrul <bodrul.ic@msn.com>
2019-03-11 16:30:33 +0000

    Added hotel repository and interface.

---
+++ Hotels.Core/Contracts/Repositories/IHotelRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hotels.Core.Models;
+
+namespace Hotels.Core.Contracts.Repositories
+{
+    /// <summary>
+    /// The hotel repository interface.
+    /// </summary>
+    public interface IHotelRepository
+    {
+        /// <summary>
+        /// Get <see cref="Hotel"/> by id.
+        /// </summary>
+        /// <returns></returns>
+        Task<Hotel> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Get list of <see cref="Hotel"/>s by matching case-insensitive string.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Hotel>> GetListByMatch(string name);
+    }
+}
---
+++ Hotels.Persistence/Repositories/HotelRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hotels.Core.Contracts.Repositories;
+using Hotels.Core.Models;
+
+namespace Hotels.Persistence.Repositories
+{
+    /// <inheritdoc />
+    public sealed class HotelRepository : IHotelRepository
+    {
+        public async Task<Hotel> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Hotel>> GetListByMatch(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
