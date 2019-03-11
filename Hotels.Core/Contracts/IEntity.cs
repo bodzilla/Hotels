519fe900436cebbcdce992eb459401394789ab53
Bodrul <bodrul.ic@msn.com>
2019-03-11 15:47:42 +0000

    Added entity interface.

---
+++ Hotels.Core/Contracts/IEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hotels.Core.Contracts
+{
+    /// <summary>
+    /// The base interface for all models.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// The id of the entity.
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// The date/time when the entity was created.
+        /// </summary>
+        DateTime EntityCreated { get; set; }
+
+        /// <summary>
+        /// The entity active state.
+        /// </summary>
+        bool EntityActive { get; set; }
+    }
+}
