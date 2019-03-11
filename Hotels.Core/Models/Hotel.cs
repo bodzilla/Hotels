05de2c174367579aead67cb705037cfa019d5d55
Bodrul <bodrul.ic@msn.com>
2019-03-11 15:49:56 +0000

    Added hotel class.

---
+++ Hotels.Core/Models/Hotel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Hotels.Core.Contracts;
+using Hotels.Core.Enums;
+
+namespace Hotels.Core.Models
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// The hotel class.
+    /// </summary>
+    public sealed class Hotel : IEntity
+    {
+        /// <inheritdoc />
+        public int Id { get; set; }
+
+        /// <inheritdoc />
+        public DateTime EntityCreated { get; set; }
+
+        /// <inheritdoc />
+        public bool EntityActive { get; set; }
+
+        /// <summary>
+        /// The hotel name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the hotel.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The location of the hotel.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// The hotel rating.
+        /// </summary>
+        public Rating Rating { get; set; }
+    }
+}
