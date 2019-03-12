019f5f0729d906aeb88bd941a19480031842ed54
Bodrul <bodrul.ic@msn.com>
2019-03-12 09:41:15 +0000

    Added all hotel service tests.

--- Hotels.UnitTests/Services/HotelServiceTests.cs
+++ Hotels.UnitTests/Services/HotelServiceTests.cs
@@ -47,5 +47,54 @@ namespace Hotels.UnitTests.Services
             Assert.That(hotels, Is.TypeOf<List<Hotel>>());
             Assert.That(hotels.Count, Is.EqualTo(3));
         }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(int.MaxValue)]
+        public async Task GetIdByAsync_GetHotelByIdAsync_ReturnsOneHotel(int id)
+        {
+            // Set up sample data.
+            var data = new Hotel
+            {
+                Id = id,
+                Name = "Name",
+                Description = "Description",
+                Location = "Location",
+                Rating = Rating.Five
+            };
+
+            // Ensure this method returns the sample data.
+            _hotelRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(data);
+
+            var result = await _hotelService.GetByIdAsync(id);
+
+            Assert.That(result, Is.TypeOf<Hotel>());
+            Assert.That(result.Id, Is.EqualTo(id));
+        }
+
+        [TestCase("Test")]
+        [TestCase("Hotel")]
+        [TestCase("Name")]
+        public async Task GetListByMatchAsync_GetHotelListByMatchingNameAsync_ReturnsThreeHotels(string name)
+        {
+            // Set up sample data.
+            var data = new List<Hotel>
+            {
+                new Hotel{Id = 1, Name = $"{name}1", Description = "Description1", Location = "Location1", Rating = Rating.Five},
+                new Hotel{Id = 2, Name = $"{name}2", Description = "Description2", Location = "Location2", Rating = Rating.Three},
+                new Hotel{Id = 3, Name = $"{name}3", Description = "Description3", Location = "Location3", Rating = Rating.One}
+            };
+
+            // Ensure this method returns the sample data.
+            _hotelRepository.Setup(x => x.GetListByMatch(It.IsAny<string>())).ReturnsAsync(data);
+
+            var result = await _hotelService.GetListByMatch(name);
+
+            // Cast to list to make assertions.
+            var hotels = result.ToList();
+
+            Assert.That(hotels, Is.TypeOf<List<Hotel>>());
+            foreach (var hotel in hotels) Assert.That(hotel.Name.Contains(name));
+        }
     }
 }
