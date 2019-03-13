22cafd348c9bc08b9b603b6c2fd3081a1ea042d6
Bodrul <bodrul.ic@msn.com>
2019-03-13 11:27:07 +0000

    Styling search.

--- Hotels.Website/ClientApp/src/components/Search.js
+++ Hotels.Website/ClientApp/src/components/Search.js
@@ -6,6 +6,27 @@ export class Search extends Component {
     constructor(props) {
         super(props);
         this.state = { loading: true };
+    }
 
+    render() {
+        let contents =
+            <form onSubmit={this.handleSubmit}>
+                <div class="form-row align-items-center">
+                    <div class="col-sm-3 my-1">
+                        <input type="text" class="form-control " value={this.state.value} onChange={this.handleChange} />
+                    </div>
+                    <div class="col-auto">
+                        <input type="submit" class="btn btn-primary" value="Search" />
+                    </div>
+                </div>
+            </form>;
+
+        return (
+            <div>
+                <h1>Search for hotels</h1>
+                <br />
+                {contents}
+            </div>
+        );
     }
 }
